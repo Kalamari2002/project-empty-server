01fdfc58f5e99d7c06cd9ae855a8424ceaba35d1
Gabriel Ikezaki <gabriel.ikezaki@gmail.com>
2025-10-28 14:20:24 -0700

    Added: machine learning env

--- Assets/Scripts/Behavior/Bot/_botMoveToTarget.cs
+++ Assets/Scripts/Behavior/Bot/_botMoveToTarget.cs
@@ -4,30 +4,31 @@ using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
 using UnityEngine.Rendering;
 
-
-/**
-* Stacked vectors: provides "memory" to the agent. For eg. if you set it to 1, the
-* agent only takes 1 observation before requesting an action. However, if you set it to 3,
-* it'll take the current position and the previous 2 positions (or observations) and then 
-* make a decision.This could help the agent to, for example, predict where the target will be
-* based on its previous positions.
-*/
-
-public class MoveToTarget : Agent
+[RequireComponent(typeof(BotMovement))]
+public class _botMoveToTarget : Agent
 {
+    float INIT_ACTION_COOLDOWN = .1f;
+
     [SerializeField] int maxStep;
     [SerializeField] Transform targetTransform;
-    [SerializeField] float speed;
     [SerializeField] MeshRenderer floorRenderer;
     [SerializeField] Color failureColor;
     [SerializeField] Color successColor;
+
+    BotMovement movement;
 
     int steps = 0;
     Vector3 startingPos;
     int actionCount = 0;
 
+    float timer = 1.0f;
+    float actionCoolDown;
+    float xInput, zInput = 0.0f;
     void Start()
     {
+        
+        actionCoolDown = INIT_ACTION_COOLDOWN;
+        movement = GetComponent<BotMovement>();
         transform.localPosition = new Vector3(Random.Range(-8.0f, 16.0f), 1.36f, Random.Range(-20f, 5.0f));
         targetTransform.localPosition = new Vector3(Random.Range(-8.0f, 16.0f), 0, Random.Range(-20f, 5.0f));
     }
@@ -37,28 +38,33 @@ public class MoveToTarget : Agent
     */
     public override void OnEpisodeBegin()
     {
+        SetReward(0.0f);
         transform.localPosition = new Vector3(Random.Range(-8.0f, 16.0f), 1.36f, Random.Range(-20f, 5.0f));
         targetTransform.localPosition = new Vector3(Random.Range(-8.0f, 16.0f), 0, Random.Range(-20f, 5.0f));
     }
-    void FixedUpdate()
-    {
-        if(steps >= 0)
-            steps++;
-        if(steps >= 500)
-        {
-            Debug.Log("Frames: " + steps + ", Action Count: " + actionCount);
-            steps = -1;
-        }
-    }
+
     void Update()
     {
         if (StepCount == maxStep)
         {
             OnTimeOver();
         }
+        Debug.Log(actionCoolDown);
+        actionCoolDown -= Time.deltaTime;
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            AddReward(-.1f);
+            timer = 0;
+        }
 
     }
 
+    void FixedUpdate()
+    {
+
+        movement.Move(xInput, zInput);
+    }
 
     // How the agent "observes its environmnet". Like inputs for the agent. "What data does your
     // model need to solve the problem?"
@@ -83,13 +89,17 @@ public class MoveToTarget : Agent
     */
     public override void OnActionReceived(ActionBuffers actions)
     {
+        if (actionCoolDown > 0) return;
         /**
         * Continuous actions is set to 2: X and Z positions
         */
-        float moveX = actions.ContinuousActions[0];
-        float moveZ = actions.ContinuousActions[1];
-        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * speed;
-        actionCount++;
+        int moveX = actions.DiscreteActions[0] - 1;
+        int moveZ = actions.DiscreteActions[1] - 1;
+
+        xInput = moveX;
+        zInput = moveZ;
+        Debug.Log("ACTION");
+        actionCoolDown = INIT_ACTION_COOLDOWN;
     }
 
     /**
@@ -97,15 +107,14 @@ public class MoveToTarget : Agent
     */
     public override void Heuristic(in ActionBuffers actionsOut)
     {
-        ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
-        continuousActions[0] = Input.GetAxisRaw("Horizontal");
-        continuousActions[1] = Input.GetAxisRaw("Vertical");
+        ActionSegment<int> discreteActions = actionsOut.DiscreteActions;
+        discreteActions[0] = (int) Input.GetAxisRaw("Horizontal");
+        discreteActions[1] = (int) Input.GetAxisRaw("Vertical");
     }
 
     private void OnTimeOver()
     {
         Debug.Log("Time OUT");
-        SetReward(-1.0f);
         EndEpisode();
     }
 
@@ -113,12 +122,12 @@ public class MoveToTarget : Agent
     {
         if (other.CompareTag("Target"))
         {
-            SetReward(1.0f);    //Success, set reward as a positive value
+            AddReward(3.0f);    //Success, set reward as a positive value
             floorRenderer.material.color = successColor;
         }
         else if (other.CompareTag("Wall"))
         {
-            SetReward(-1.0f);   //Failure, set reward as a negative value
+            AddReward(-1.0f);   //Failure, set reward as a negative value
             floorRenderer.material.color = failureColor;
         }
         else
