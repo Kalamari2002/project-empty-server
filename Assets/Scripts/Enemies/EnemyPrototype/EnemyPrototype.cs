a61b4f8464cb982f08bc243a43b7359126acfff7
mateus-ikezaki <mateus_ikezaki@berkeley.edu>
2026-03-07 14:04:33 -0800

    adding enemy idle animation

--- Assets/Scripts/Enemies/EnemyPrototype/EnemyPrototype.cs
+++ Assets/Scripts/Enemies/EnemyPrototype/EnemyPrototype.cs
@@ -1,12 +1,17 @@
 using UnityEngine;
+using UnityEngine.Video;
 
 public class EnemyPrototype : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] float backOffDistance;
     [SerializeField] float stopDistance;
+    [SerializeField] VideoClip runningClip;
+    [SerializeField] VideoClip idleClip;
+    [SerializeField] VideoPlayer videoPlayer;
     Transform Player;
     CharacterController controller;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -28,10 +33,22 @@ public class EnemyPrototype : MonoBehaviour
         if (Vector3.Distance(transform.position, Player.transform.position) >= stopDistance)
         {
             controller.SimpleMove(transform.forward * speed);
+            videoPlayer.clip = runningClip;
+            videoPlayer.Play();
+            videoPlayer.playbackSpeed = 1;
         }
         else if (Vector3.Distance(transform.position, Player.transform.position) <= backOffDistance)
         {
             controller.SimpleMove(-transform.forward * speed/2);
+            videoPlayer.clip = runningClip;
+            videoPlayer.Play();
+            videoPlayer.playbackSpeed = 1;
+        }
+        else
+        {
+            videoPlayer.clip = idleClip;
+            videoPlayer.Play();
+            videoPlayer.playbackSpeed = 0.5f;
         }
     }
 }
