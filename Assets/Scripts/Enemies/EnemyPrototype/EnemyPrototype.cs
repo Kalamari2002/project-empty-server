aa099a5dfc1d802409e930d6b50343c7ed0aa49f
mateus-ikezaki <mateus_ikezaki@berkeley.edu>
2026-03-22 13:52:09 -0700

    ragdoll added to png enemy

--- Assets/Scripts/Enemies/EnemyPrototype/EnemyPrototype.cs
+++ Assets/Scripts/Enemies/EnemyPrototype/EnemyPrototype.cs
@@ -9,6 +9,15 @@ public class EnemyPrototype : MonoBehaviour
     [SerializeField] VideoClip runningClip;
     [SerializeField] VideoClip idleClip;
     [SerializeField] VideoPlayer videoPlayer;
+
+    [Header("Ragdoll")]
+    [SerializeField] GameObject Ragdoll;
+    [SerializeField] float ragdollLaunchForce;
+    [SerializeField] float ragdollDuration;
+    [SerializeField] bool ragdollMode = false;
+    [SerializeField] GameObject ActiveRagdoll;
+    float ragdollCountDown;
+
     Transform Player;
     CharacterController controller;
 
@@ -17,17 +26,56 @@ public class EnemyPrototype : MonoBehaviour
     {
         Player = GameObject.FindGameObjectWithTag("Player").transform;
         controller = GetComponent<CharacterController>();
+        ragdollCountDown = ragdollDuration;
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        HandleRagdoll();
+        if (Input.GetMouseButtonDown(0))
+        {
+            SpawnRagdoll();
+        }
+    }
+
+    void SpawnRagdoll()
+    {
+        if (ActiveRagdoll == null)
+        {
+            ActiveRagdoll = Instantiate(Ragdoll, transform.position, Quaternion.identity);
+            ActiveRagdoll.transform.forward = transform.forward;
+        }
+        Rigidbody ragdollRb = ActiveRagdoll.GetComponent<Rigidbody>();
+        ragdollRb.AddForce((Player.transform.forward + Vector3.up).normalized * ragdollLaunchForce, ForceMode.Impulse);
+        ragdollRb.AddTorque((Player.transform.forward + Vector3.up).normalized * ragdollLaunchForce, ForceMode.Impulse);
+        ragdollMode = true;
+        ragdollCountDown = ragdollDuration;
+
+        videoPlayer.enabled = false;
+        controller.enabled = false;
+    }
+
+    void HandleRagdoll()
+    {
+        if (!ragdollMode) return;
+        ragdollCountDown -= Time.deltaTime;
+        if (ragdollCountDown <= 0)
+        {
+            transform.position = new Vector3(ActiveRagdoll.transform.position.x, transform.position.y, ActiveRagdoll.transform.position.z);
+            ragdollCountDown = ragdollDuration;
+            Destroy(ActiveRagdoll);
+            ActiveRagdoll = null;
+            controller.enabled = true;
+            videoPlayer.enabled = true;
+            ragdollMode = false;
+        }
     }
 
     void Move()
     {
-        if (Player == null) { return; }
+        if (Player == null || ragdollMode) { return; }
 
         transform.LookAt(new Vector3(Player.position.x, transform.position.y, Player.position.z));
         if (Vector3.Distance(transform.position, Player.transform.position) >= stopDistance)
