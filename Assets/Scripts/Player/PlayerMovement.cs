b59f9e884e63578e16fa83252d42920de5f920fb
Gabriel Ikezaki <gabriel.ikezaki@gmail.com>
2026-04-19 00:03:20 +0100

    feature: added basic wall run to player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,11 +8,15 @@ public class PlayerMovement : MonoBehaviour
 
     [Header ("Movement Settings")]
     [SerializeField] float groundSpeed;
+    [SerializeField] float airSpeed;
     [SerializeField] float jumpForce;
     [SerializeField] float airMultiplier = .5f;
     [SerializeField] float maxGroundSpeed;
     [SerializeField] float maxAirSpeed;
     [SerializeField] float drag;
+
+    PlayerWallRun wallRun; 
+    PlayerAim playerAim;
 
     Rigidbody rb;
     Transform orientation;
@@ -20,6 +24,8 @@ public class PlayerMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        playerAim = GetComponent<PlayerAim>();
+        wallRun = GetComponent<PlayerWallRun>();
         rb = GetComponent<Rigidbody>();
         orientation = transform.Find("Orientation");
     }
@@ -41,18 +47,47 @@ public class PlayerMovement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
         float multiplier = Grounded() ? 1 : airMultiplier;
         Vector3 directionVector = orientation.forward * vertical + orientation.right * horizontal;
-
-        rb.AddForce(directionVector.normalized * groundSpeed * multiplier);
+    
+        rb.AddForce(directionVector.normalized * (Grounded() ? groundSpeed : airSpeed) * multiplier);
         rb.linearDamping = Grounded() ? drag : 0;
-
+        
+        WallRun(horizontal);
+        
         LimitSpeed();
+    }
+    void WallRun(float horizontal)
+    {
+
+        if (Grounded())
+        {
+            wallRun.SetCanWallRun(false);
+            wallRun.SetIsWallRunning(false);
+            return;
+        }
+
+        wallRun.SetCanWallRun(true);
+
+        if(wallRun.TouchingWall() && (horizontal != 0)){
+            wallRun.WallRun();
+            wallRun.SetIsWallRunning(true);
+        }
+        else
+        {
+            wallRun.SetIsWallRunning(false);   
+        }
     }
 
     void Jump()
     {
-        if (!Grounded()) return;
+        if (!Grounded() && !wallRun.GetIsWallRunning()) return;
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            if (wallRun.GetIsWallRunning())
+            {
+                wallRun.WallJump(playerAim.cameraForward());
+                return;
+            }
+            
             rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
@@ -62,7 +97,7 @@ public class PlayerMovement : MonoBehaviour
     {
         float speedLimit = Grounded() ? maxGroundSpeed : maxAirSpeed;
         Vector3 xzVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
-        if (xzVelocity.magnitude > maxGroundSpeed)
+        if (xzVelocity.magnitude > speedLimit)
         {
             xzVelocity = xzVelocity.normalized * speedLimit;
             rb.linearVelocity = new Vector3(xzVelocity.x, rb.linearVelocity.y, xzVelocity.z);
