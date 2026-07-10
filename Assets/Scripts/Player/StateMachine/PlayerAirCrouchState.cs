ae193daaef739d3263d8324a28e4bd63d66ac4ad
Gabriel Ikezaki <gabriel.ikezaki@gmail.com>
2026-07-10 23:17:30 +0100

    feat: introduce orientation animations for crouching

--- Assets/Scripts/Player/StateMachine/PlayerAirCrouchState.cs
+++ Assets/Scripts/Player/StateMachine/PlayerAirCrouchState.cs
@@ -21,12 +21,14 @@ public class PlayerAirCrouchState : PlayerBaseState
     public override void FixedUpdateState(){}
     public override void ExitState()
     {
-        _context.StandUp();
+        // _context.StandUp();
     }
     public override void CheckSwitchStates()
     {
         if (!_context.IsCrouchPressed)
         {
+            _context.OrientationAnimator.SetTrigger("AirStandUp");
+            _context.OrientationAnimator.SetBool("AirCrouched", false);
             SwitchState(_factory.Freefall());
         }
     }
@@ -34,22 +36,24 @@ public class PlayerAirCrouchState : PlayerBaseState
 
     void Crouch()
     {
-        CapsuleCollider collision = _context.CollisionCapsule;
+        _context.OrientationAnimator.SetTrigger("AirCrouch");
+        _context.OrientationAnimator.SetBool("AirCrouched", true);
+        // CapsuleCollider collision = _context.CollisionCapsule;
 
-        collision.height = _context.CROUCH_COLLISION_HEIGHT;
-        Transform groundCheck = _context.GroundCollision;
-        groundCheck.localPosition = new Vector3(
-            groundCheck.localPosition.x, 
-            AIR_CROUCH_GROUNDCHECK_Y, 
-            groundCheck.localPosition.z
-        );
+        // collision.height = _context.CROUCH_COLLISION_HEIGHT;
+        // Transform groundCheck = _context.GroundCollision;
+        // groundCheck.localPosition = new Vector3(
+        //     groundCheck.localPosition.x, 
+        //     AIR_CROUCH_GROUNDCHECK_Y, 
+        //     groundCheck.localPosition.z
+        // );
 
-        _context.CameraTransform.localPosition = _context.InitCameraPos;
+        // _context.CameraTransform.localPosition = _context.InitCameraPos;
 
-        collision.center = new Vector3(
-            collision.center.x, 
-            _context.CROUCH_COLLISION_CENTER_Y, 
-            collision.center.z
-        );
+        // collision.center = new Vector3(
+        //     collision.center.x, 
+        //     _context.CROUCH_COLLISION_CENTER_Y, 
+        //     collision.center.z
+        // );
     }
 }
--- Assets/Scripts/Player/StateMachine/PlayerFreefallState.cs
+++ Assets/Scripts/Player/StateMachine/PlayerFreefallState.cs
@@ -3,22 +3,15 @@ using UnityEngine;
 
 public class PlayerFreefallState : PlayerBaseState
 {
-    float _crouchTimer;
     public PlayerFreefallState(PlayerStateMachine context, PlayerStateFactory factory)
     :base(context, factory)
     {
         name = "Freefall";
-        _crouchTimer = -1.0f;
     }
 
     public override void EnterState(){}
     public override void UpdateState()
     {
-        if(_crouchTimer >= 0.0f)
-        {
-            _crouchTimer -= Time.deltaTime;
-            Debug.Log(_crouchTimer);   
-        }
         CheckSwitchStates();
     }
     public override void FixedUpdateState(){}
@@ -30,11 +23,8 @@ public class PlayerFreefallState : PlayerBaseState
     public override void InitializeSubState(){}
     void Crouch()
     {
-        if (_context.ReleasedCrouch)
-        {
-            _crouchTimer = _context.CROUCH_COOLDOWN;
-        }
-        if (_context.PressedCrouch && _crouchTimer <= 0.0f)
+
+        if (_context.PressedCrouch)
         {
             SwitchState(_factory.AirCrouch());
         }
